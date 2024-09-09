ba6cbfd61cd765095ac53e9d21268fe7a13aa174
Aniruddh Joshi <118598273+aniruddh-joshi@users.noreply.github.com>
2024-09-09 08:48:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+        vector<int>freq(3,0);
+        for(int i=0;i<arr.size();i++)
+        freq[arr[i]]++;
+        int idx=0;
+        for(int i=0;i<3;i++){
+            int num=freq[i];
+            for(int j=0;j<num;j++)
+            arr[idx++]=i;
+        }
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        vector<int> a;
+        string input;
+        int num;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            a.push_back(num);
+        }
+        Solution ob;
+        ob.sort012(a);
+
+        int n = a.size();
+        for (int i = 0; i < n; i++) {
+            cout << a[i] << " ";
+        }
+
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
